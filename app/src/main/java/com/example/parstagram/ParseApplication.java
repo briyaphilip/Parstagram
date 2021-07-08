1039022e72ae5a9d06fbb51ebd8c362dbe17350d
Briya Philip <briya@fb.com>
2021-07-08 17:03:42 -0400

    Implemented camera and post saving functionality

--- app/src/main/java/com/example/parstagram/ParseApplication.java
+++ app/src/main/java/com/example/parstagram/ParseApplication.java
@@ -3,12 +3,15 @@ package com.example.parstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("eLpkPo8JOuGDki1yLSMNAnva0dSNRsmSW7f7b8FL")
